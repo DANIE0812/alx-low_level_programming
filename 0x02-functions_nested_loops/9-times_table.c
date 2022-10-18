48573190cb35c2a2bf78ed07dde8258aead69348
Daniel Effiom <danie0812.ds@gmail.com>
2022-10-18 12:20:52 -0700

    11. 98 Battery Street, the OG

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -1,48 +1,30 @@
 #include "main.h"
 
 /**
-* times_table -prints tables
-* Return: Always 0.
-**/
-
+ * times_table -prints tables
+ * Return: Always 0.
+ **/
 void times_table(void)
-
 {
-	int a;
-	int b;
-	int c;
-	for (a = 0; a <= 9; a++)
+	int num, mult, prod;
+
+	for (num = 0; num <= 9; num++)
 	{
-		for (b = 0; b <= 9; b++)
+		_putchar('0');
+
+		for (mult = 1; mult <= 9; mult++)
 		{
-			c = a * b;
-			if ((c / 10) == 0)
-			{
-				if (b == 0)
-				{
-					_putchar ('0');
-				}
-				if (b != 0)
-				{
-					_putchar (' ');
-					_putchar ((c % 10) + '0');
-				}
-				if (b < 9)
-				{
-					_putchar(',');
-					_putchar (' ');
-				}
-			}			else
-			{
-				_putchar ((c / 10) + '0');
-				_putchar ((c % 10) + '0');
-				if (b < 9)
-				{
-					_putchar(',');
-					_putchar (' ');
-				}
-			}
+			_purchar(',');
+			_purchar(' ');
+
+			prod = num * mult;
+
+			if (prod <= 9)
+				_purchar(' ');
+			else
+				_purchar((prod / 10) + '0');
+			_purchar((prod % 10) + '0');
 		}
-		_putchar ('\n');
+		_purchar('\n');
 	}
 }
