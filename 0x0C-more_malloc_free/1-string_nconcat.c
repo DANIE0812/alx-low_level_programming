04b29a4ea9a3b29083994a1536ce745716da4326
Daniel Effiom <danie0812.ds@gmail.com>
2022-11-11 19:22:01 -0800

    Update

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -22,19 +22,20 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	{
 		s2 = "";
 	}
+
 	while (s1[size1] != '\0')
 	{
 		size1++;
 	}
+
 	while (s2[size2] != '\0')
 	{
 		size2++;
 	}
 	if (n > size2)
-	{
-		n = size2;
-		p = malloc((size1 + n + 1) * sizeof(char));
-	}
+	n = size2;
+	p = malloc((size1 + n + 1) * sizeof(char));
+
 	if (p == NULL)
 	{
 		return (0);
