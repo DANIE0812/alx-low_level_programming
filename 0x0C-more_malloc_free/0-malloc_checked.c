454f72311390e2718d05a2666c2428bc94e58d96
Daniel Effiom <danie0812.ds@gmail.com>
2022-11-11 19:00:10 -0800

    Task 0. Trust no one

--- 0x0C-more_malloc_free/0-malloc_checked.c
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -1,4 +1,5 @@
 #include "main.h"
+#include <stdlib.h>
 
 /**
  * malloc_checked - array for prints a string
@@ -8,13 +9,12 @@
 
 void *malloc_checked(unsigned int b)
 {
-	void *p;
-
-	*p = malloc(b);
+	void *p = malloc(b);
 
 	if (p == NULL)
 	{
 		exit(98);
 	}
+
 	return (p);
 }
